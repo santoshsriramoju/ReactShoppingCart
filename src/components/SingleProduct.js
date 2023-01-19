097919f116d41009ff59bed2b97f2ae12b98d336
santoshsriramoju <santoshsriramoju2691@gmail.com>
2023-01-19 17:51:11 +0530

    Initial Commit

---
+++ src/components/SingleProduct.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { CartState } from '../context/Context';
+import Rating from './Rating';
+
+const SingleProduct = ({ prod }) => {
+    const { state: { cart }, dispatch } = CartState();
+    return (
+        <div className="products">
+            <Card>
+                <Card.Img variant="top" src={prod.image} alt={prod.name} />
+                <Card.Body>
+                    <Card.Title>{prod.name}</Card.Title>
+                    <Card.Subtitle style={{ paddingBottom: 10 }}>
+                        <span>₹ {prod.price.split(".")[0]}</span>
+                        {
+                            prod.fastDelivery ? (
+                                <div>Fast Delivery</div>
+                            ) : (
+                                <div>4 Days Delivery</div>
+                            )
+                        }
+                        <Rating rating={prod.ratings} />
+                    </Card.Subtitle>
+                    {
+                        cart.some(p => p.id === prod.id) ? (
+                            <Button onClick={() => {
+                                dispatch({
+                                    type: "REMOVE_FROM_CART",
+                                    payload: prod
+                                })
+                            }} size="sm" variant="danger">Remove From Cart</Button>
+                        ) : (
+                            <Button onClick={() => {
+                                dispatch({
+                                    type: "ADD_TO_CART",
+                                    payload: prod
+                                })
+                            }} size="sm" disabled={!prod.inStock}>{!prod.inStock ? "Out of stock" : "Add to cart"}</Button>
+                        )
+                    }
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
+
+export default SingleProduct
